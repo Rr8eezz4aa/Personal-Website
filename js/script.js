eabf88984d038af336d189cafe1007ec9a8c7d6a
Reza Rafsanjani <rr8eezz4aa@gmail.com>
2020-08-03 00:21:56 +0430

    improve styles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 window.onscroll = function() {scrollFunction()}
+window.onload = () => {window.scrollTo(0,0);}
 
 function scrollFunction() {
     if (document.body.scrollTop > window.innerHeight/2 || document.documentElement.scrollTop > window.innerHeight/2) {
